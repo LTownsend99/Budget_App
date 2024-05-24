87215ff4ec6df2ca209d3e9eec07e5fd00ee012c
Liam Townsend <L.Townsend7628@student.leedsbeckett.ac.uk>
2024-05-24 14:20:16 +0100

    add second page and selection bar

---
+++ lib/pages/budgeting.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BudgetingPage extends StatefulWidget
+{
+  const BudgetingPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,6 +16,7 @@ class _MyHomePageState extends State<HomePage> {
   final newExpenseNameController = TextEditingController();
   final newExpensePoundController = TextEditingController();
   final newExpensePenceController = TextEditingController();
+  int selectedIndex = 0;
 
   @override
   void initState() {
@@ -24,86 +25,83 @@ class _MyHomePageState extends State<HomePage> {
     Provider.of<ExpenseData>(context, listen: false).prepareData();
   }
 
-  //addNewExpense Method
+  // Add new expense method
   void addNewExpense() {
     showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-              title: Text('Add New Expense'),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  //expense name
-                  TextField(
-                    controller: newExpenseNameController,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add New Expense'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Expense name
+            TextField(
+              controller: newExpenseNameController,
+              decoration: const InputDecoration(
+                hintText: "Expense Name",
+              ),
+            ),
+            // Expense amount
+            Row(
+              children: [
+                // Pounds
+                Expanded(
+                  child: TextField(
+                    controller: newExpensePoundController,
+                    keyboardType: TextInputType.number,
                     decoration: const InputDecoration(
-                      hintText: "Expense Name",
+                      hintText: "Pounds",
                     ),
                   ),
-                  // expense amount
-                  Row(
-                    children: [
-                      //pounds
-                      Expanded(
-                        child: TextField(
-                          controller: newExpensePoundController,
-                          keyboardType: TextInputType.number,
-                          decoration: const InputDecoration(
-                            hintText: "Pounds",
-                          ),
-                        ),
-                      ),
-
-                      //pence
-                      Expanded(
-                        child: TextField(
-                          controller: newExpensePenceController,
-                          keyboardType: TextInputType.number,
-                          decoration: const InputDecoration(
-                            hintText: "Pence",
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  // expense date
-                ],
-              ),
-              actions: [
-                //save button
-                MaterialButton(
-                  onPressed: save,
-                  child: Text('Save'),
                 ),
-
-                //cancel button
-                MaterialButton(
-                  onPressed: cancel,
-                  child: Text('Cancel'),
-                )
+                // Pence
+                Expanded(
+                  child: TextField(
+                    controller: newExpensePenceController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      hintText: "Pence",
+                    ),
+                  ),
+                ),
               ],
-            ));
+            ),
+            // Expense date
+          ],
+        ),
+        actions: [
+          // Save button
+          MaterialButton(
+            onPressed: save,
+            child: Text('Save'),
+          ),
+          // Cancel button
+          MaterialButton(
+            onPressed: cancel,
+            child: Text('Cancel'),
+          ),
+        ],
+      ),
+    );
   }
 
-  // delete expense
-
+  // Delete expense method
   void deleteExpense(ExpenseItem expenseItem) {
     Provider.of<ExpenseData>(context, listen: false).deleteExpense(expenseItem);
   }
 
+  // Save method
   void save() {
     if (newExpenseNameController.text.isNotEmpty &&
         newExpensePoundController.text.isNotEmpty &&
         newExpensePenceController.text.isNotEmpty) {
-
       String amount =
-          newExpensePoundController.text + '.' + newExpensePenceController.text;
+          '${newExpensePoundController.text}.${newExpensePenceController.text}';
       ExpenseItem newExpenseItem = ExpenseItem(
         name: newExpenseNameController.text,
         amount: amount,
         dateTime: DateTime.now(),
       );
-
       Provider.of<ExpenseData>(context, listen: false)
           .addNewExpense(newExpenseItem);
     }
@@ -112,47 +110,80 @@ class _MyHomePageState extends State<HomePage> {
     clear();
   }
 
+  // Cancel method
   void cancel() {
     Navigator.pop(context);
     clear();
   }
 
+  // Clear method
   void clear() {
     newExpensePoundController.clear();
     newExpensePenceController.clear();
     newExpenseNameController.clear();
   }
 
+  // Method to handle item tap in bottom navigation bar
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<ExpenseData>(
-      builder: (context, value, child) => Scaffold(
-          backgroundColor: Colors.white,
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.blue,
-            onPressed: addNewExpense,
-            child: Icon(Icons.add),
-          ),
-          body: ListView(
-            children: [
-              //weekly summary
-              ExpenseSummary(startOfWeek: value.startOfWeekDate()),
-
-              //expense List
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: value.getOverallExpenses().length,
-                itemBuilder: (context, index) => ExpenseTile(
-                  name: value.getOverallExpenses()[index].name,
-                  amount: value.getOverallExpenses()[index].amount,
-                  dateTime: value.getOverallExpenses()[index].dateTime,
-                  deleteTapped: (p0) =>
-                      deleteExpense(value.getOverallExpenses()[index]),
-                ),
+    List<Widget> pages = [
+      Consumer<ExpenseData>(
+        builder: (context, value, child) => ListView(
+          children: [
+            // Weekly summary
+            ExpenseSummary(startOfWeek: value.startOfWeekDate()),
+            const SizedBox(height: 10),
+            // Expense list
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: value.getOverallExpenses().length,
+              itemBuilder: (context, index) => ExpenseTile(
+                name: value.getOverallExpenses()[index].name,
+                amount: value.getOverallExpenses()[index].amount,
+                dateTime: value.getOverallExpenses()[index].dateTime,
+                deleteTapped: (p0) =>
+                    deleteExpense(value.getOverallExpenses()[index]),
               ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
+      Placeholder(), // This can be your Budgeting page
+    ];
+
+    List<String> pageTitles = [
+      'Home',
+      'Budgeting',
+    ];
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: addNewExpense,
+        child: Icon(Icons.add),
+      ),
+      body: pages[selectedIndex],
+      appBar: AppBar(
+        title: Text(pageTitles[selectedIndex]),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.blue,
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.money), label: 'Budgeting'),
+        ],
+      ),
     );
   }
 }
